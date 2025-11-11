415d225963c4e3b1de19dca729fc7cafebe8b0d1
Muhammad isnaini <68492618+isnakode@users.noreply.github.com>
2025-11-11 18:53:28 +0800

    add deploy.yaml

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/vlowchart/" : "/",
   plugins: [
     vue(),
     tw(),
